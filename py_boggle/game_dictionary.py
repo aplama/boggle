97599cd75ad578c905a117c7974760439eb98d6f
Andrei Plama <aplama@protonmail.com>
2022-03-04 00:44:20 -0500

    updated game_dict

--- py_boggle/game_dictionary.py
+++ py_boggle/game_dictionary.py
@@ -10,16 +10,36 @@ class GameDictionary(BoggleDictionary):
     """
 
     def __init__(self):
-        raise NotImplementedError("method __init__") # TODO: implement your code here
+        WORDS_FILE = "words.txt"
+        self.game_dict = {}
+        self.words = set()
+        with open(WORDS_FILE) as fin:
+            for line in fin:
+                line = line.strip().upper()
+                self.words.add(line)
+        # raise NotImplementedError("method __init__") # TODO: implement your code here
 
     def load_dictionary(self, filename: str) -> None:
-        raise NotImplementedError("method load_dictionary") # TODO: implement your code here
+        
+        with open(filename) as wordfile:
+            for line in wordfile:
+                word = line.strip().upper()
+                if len(word) > 0:
+                    self.game_dict[word] = len(word)
+                    
+        # raise NotImplementedError("method load_dictionary") # TODO: implement your code here
 
     def is_prefix(self, prefix: str) -> bool:
         raise NotImplementedError("method is_prefix") # TODO: implement your code here
 
     def contains(self, word: str) -> bool:
-        raise NotImplementedError("method contains") # TODO: implement your code here
+        # return word in self.words
+        for word in self.words:
+            if word in self.game_dict:
+                return True
+            else:
+                return False
+        # raise NotImplementedError("method contains") # TODO: implement your code here
 
     def __iter__(self) -> typing.Iterator[str]:
         raise NotImplementedError("method __iter__") # TODO: implement your code here
